2c3de30f9e2f2f18f22035f779c26b06b7cb3634
Aleksandar Zivkovic <zivkovic.aleks@gmail.com>
2022-03-14 21:01:33 +0100

    Class04 update

--- SEDC.Oop.Class04/SEDC.Oop.Class04.Exercise/Program.cs
+++ SEDC.Oop.Class04/SEDC.Oop.Class04.Exercise/Program.cs
@@ -6,60 +6,79 @@ namespace SEDC.Oop.Class04.Exercise
     {
         static void Main(string[] args)
         {
+            //EXERCISE 4
 
-            //int result = Sum(10, 5);
-            //Console.WriteLine(result);
-
-            Console.WriteLine("Enter first number");
-            string userInputOne = Console.ReadLine();
-            bool isValidInputOne = int.TryParse(userInputOne, out int parsedInputOne);
-
-            Console.WriteLine("Enter second number");
-            string userInputTwo = Console.ReadLine();
-            bool isValidInputTwo = int.TryParse(userInputOne, out int parsedInputTwo);
-
-            Console.WriteLine("If you want to multiply press *, if you want to subtract press -");
-            string choice = Console.ReadLine();
-
-            if(choice == "*")
-            {
-                Multiply(parsedInputOne, parsedInputTwo);
-            }
-            else if(choice == "-")
-            {
-                Substract(parsedInputOne, parsedInputTwo);
-            }
+            DateTime currentDay = DateTime.Now;
+            DateTime addeaddedDays = currentDay.AddDays(5);
 
 
-            // parsing values
 
-            //if (userInput == "+")
-            //{
-            //    Sum(1, 2); // SUm(num1, num2);
-            //    Console.WriteLine();
-            //}
+            Console.WriteLine(currentDay);
+
+
+
+
+
+
+
+
+        //    //int result = Sum(10, 5);
+        //    //Console.WriteLine(result);
+
+        //    Console.WriteLine("Enter first number");
+        //    string userInputOne = Console.ReadLine();
+        //    bool isValidInputOne = int.TryParse(userInputOne, out int parsedInputOne);
+
+        //    Console.WriteLine("Enter second number");
+        //    string userInputTwo = Console.ReadLine();
+        //    bool isValidInputTwo = int.TryParse(userInputOne, out int parsedInputTwo);
+
+        //    Console.WriteLine("If you want to multiply press *, if you want to subtract press -");
+        //    string choice = Console.ReadLine();
+
+        //    if(choice == "*")
+        //    {
+        //        Multiply(parsedInputOne, parsedInputTwo);
+        //    }
+        //    else if(choice == "-")
+        //    {
+        //        Substract(parsedInputOne, parsedInputTwo);
+        //    }
+
+
+        //    // parsing values
+
+        //    //if (userInput == "+")
+        //    //{
+        //    //    Sum(1, 2); // SUm(num1, num2);
+        //    //    Console.WriteLine();
+        //    //}
             
-        }
-        static int Multiply(int a, int b)
-        {
-            int result = a * b;
-            Console.WriteLine("The result is: " + result);
-            return result;
-        }
-        static int Substract(int a, int b)
-        {
-            int result = a - b;
-            Console.WriteLine("The reslilt is: " + result);
-            return result;
+        //}
+        //static int Multiply(int a, int b)
+        //{
+        //    int result = a * b;
+        //    Console.WriteLine("The result is: " + result);
+        //    return result;
+        //}
+        //static int Substract(int a, int b)
+        //{
+        //    int result = a - b;
+        //    Console.WriteLine("The reslilt is: " + result);
+        //    return result;
 
 
 
 
-        }    //EXERCISE 3
-        static string (int num)
-        {
-            string message = "Hello from SEDC Codeacademy 2021";
-            
+        //}    //EXERCISE 3
+        //static string (int num)
+        //{
+        //    string message = "Hello from SEDC Codeacademy 2021";
+
+        
+
+        
+    
         }
 
     }   
