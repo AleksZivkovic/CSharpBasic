d4c9381cd68c4921148236914915aa94aec36e00
Aleksandar Zivkovic <zivkovic.aleks@gmail.com>
2022-03-21 20:39:57 +0100

    Class06

---
+++ SEDC.Oop.Class06.Exercises/SEDC.Oop.Class06.Exercises/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SEDC.Oop.Class06.Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberStats(5.5);
+            Console.WriteLine("Please enter a number:");
+            //double num = Console.ReadLine();
+        }
+        public static void NumberStats(double num)
+        {                            
+
+            if (num < 0)
+            {
+                Console.WriteLine("The number is NEGATIVE"); 
+            }
+            if (num > 0)
+            {
+                Console.WriteLine("The number is POSITIVE");
+            }
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("The number is EVEN");
+            }
+            else 
+            {
+                Console.WriteLine("The number is ODD");
+            }
+            if (num % 1 > 0)
+            {
+                Console.WriteLine("The number is DECIMAL");
+            }
+            else
+            {
+                Console.WriteLine("The number is INTEGER");
+            }
+            
+
+            
+        }
+
+        
+
+       
+        
+
+    }
+}
