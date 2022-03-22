1b07ca3ff0dfd93c6beb0cd41f62e029e34899ea
Aleksandar Zivkovic <zivkovic.aleks@gmail.com>
2022-03-22 19:18:45 +0100

    Homework Class02

---
+++ SEDC.Oop.Class02/SEDC.Oop.Class02.Homeworks/SEDC.Oop.Class02.Homeworks.AverageNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SEDC.Oop.Class02.Homeworks.AverageNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 2
+            //Create new console application “AverageNumber” that takes four numbers as input to calculate and print the average.
+
+            Console.WriteLine("The app name is AverageNumber");
+
+            Console.WriteLine("Please enter the first number!");
+            string input1 = Console.ReadLine();
+            bool validInteger1 = int.TryParse(input1, out int parsedInput1);
+
+            Console.WriteLine("Please enter the second number!");
+            string input2 = Console.ReadLine();
+            bool validInteger2 = int.TryParse(input2, out int parsedInput2);
+
+            Console.WriteLine("Please enter the third number!");
+            string input3 = Console.ReadLine();
+            bool validInteger3 = int.TryParse(input3, out int parsedInput3);
+
+            Console.WriteLine("Please enter the fourth number!");
+            string input4 = Console.ReadLine();
+            bool validInteger4 = int.TryParse(input4, out int parsedInput4);
+
+            int w = parsedInput1;
+            int a = parsedInput2;
+            int s = parsedInput3;
+            int d = parsedInput4;
+
+            int result = (w + a + s + d) / 4;
+            Console.WriteLine(result);
+        }
+
+    }
+}
---
+++ SEDC.Oop.Class02/SEDC.Oop.Class02.Homeworks/SEDC.Oop.Class02.Homeworks.RealCalculator/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace SEDC.Oop.Class02.Homeworks.RealCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1
+            //Create new console application “RealCalculator” that takes two numbers from the input and
+            //asks what operation would the user want to be done( +, - , * , / ). Then it returns the result.
+
+            Console.WriteLine("The app name is RealCalculator");
+
+            Console.WriteLine("Please enter first number!");
+            string input1 = Console.ReadLine();
+            bool valindInteger1 = int.TryParse(input1, out int parsedInput1);
+
+            Console.WriteLine("Please enter second number!");
+            string input2 = Console.ReadLine();
+            bool valindInteger2 = int.TryParse(input2, out int parsedInput2);
+
+            char opertaion;
+            Console.WriteLine("Please enter Operation. Example +, -, *, /");
+            opertaion = Convert.ToChar(Console.ReadLine());
+
+            if (opertaion == '+')
+            {
+                int x = parsedInput1;
+                int y = parsedInput2;
+                int result = x + y;
+                Console.WriteLine(result);
+            }
+            else if (opertaion == '-')
+            {
+                int x = parsedInput1;
+                int y = parsedInput2;
+                int result = x - y;
+                Console.WriteLine(result);
+            }
+            else if (opertaion == '*')
+            {
+                int x = parsedInput1;
+                int y = parsedInput2;
+                int result = x * y;
+                Console.WriteLine(result);
+            }
+            else if (opertaion == '/')
+            {
+                int x = parsedInput1;
+                int y = parsedInput2;
+                int result = x / y;
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid Opertation!");
+            }
+        }
+    }
+}
