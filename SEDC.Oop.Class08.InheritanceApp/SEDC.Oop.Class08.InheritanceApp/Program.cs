f23374f92919dd1ebdc3fd343d7b3cc52beb30a5
Alez1v <zivkovic.aleks@gmail.com>
2022-03-27 23:28:42 +0200

    Again

--- SEDC.Oop.Class08.InheritanceApp/SEDC.Oop.Class08.InheritanceApp/Program.cs
+++ SEDC.Oop.Class08.InheritanceApp/SEDC.Oop.Class08.InheritanceApp/Program.cs
@@ -7,6 +7,7 @@ namespace SEDC.Oop.Class08.InheritanceApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World again");
         }
     }
 }
