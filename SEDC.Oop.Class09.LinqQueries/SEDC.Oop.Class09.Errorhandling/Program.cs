9c4d77d369e800b6be4c072b07104b703e2e55ce
Aleksandar Zivkovic <zivkovic.aleks@gmail.com>
2022-03-28 20:43:31 +0200

    Class09Error

---
+++ SEDC.Oop.Class09.LinqQueries/SEDC.Oop.Class09.Errorhandling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SEDC.Oop.Class09.Errorhandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
